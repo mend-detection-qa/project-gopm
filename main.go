a15292c63f9d65d330ae878c289e004fbb56a74c
Palina Supranovich <palina.supranovich@mend.io>
2026-04-01 20:53:53 +0200

    Expand main.go above 512B minimum for Mend source file scanning

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/fatih/color"
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+)
 
 func main() {
 	color.Green("Hello from project-gopm!")
+	printInfo()
+}
+
+func printInfo() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+	fmt.Printf("Running on host: %s\n", hostname)
+	fmt.Printf("Go package manager: gopm\n")
+	fmt.Printf("Dependency file: .gopmfile\n")
+	fmt.Printf("Install command: gopm get\n")
+	fmt.Printf("Gopm uses a .gopmfile (INI format) to declare dependencies.\n")
+	fmt.Printf("Lock file: not supported (versions pinned via @ tags in .gopmfile)\n")
+	fmt.Printf("Vendor dir: vendor/ (populated by gopm get)\n")
 }
